5d8d6e97e5236c7c109730f22aebfd593ab7064e
sweeneyb <tab126@gmail.com>
2015-01-29 23:25:20 -0500

    server side added

--- src/com/sweeneyb/alljoyn/Start.java
+++ src/com/sweeneyb/alljoyn/Start.java
@@ -1,13 +1,16 @@
 package com.sweeneyb.alljoyn;
 
 import org.alljoyn.bus.BusAttachment;
-import org.alljoyn.bus.BusException;
-import org.alljoyn.bus.ProxyBusObject;
-import org.alljoyn.bus.Status;
 import org.alljoyn.bus.BusAttachment.RemoteMessage;
+import org.alljoyn.bus.BusException;
+import org.alljoyn.bus.Mutable;
+import org.alljoyn.bus.ProxyBusObject;
+import org.alljoyn.bus.SessionOpts;
+import org.alljoyn.bus.SessionPortListener;
+import org.alljoyn.bus.Status;
 
 public class Start {
-	
+
 	public static String APPNAME = "testApp";
 	public static String SERVICE_NAME = "com.sweeneyb.alljoyn.hello";
 
@@ -16,7 +19,7 @@ public class Start {
 		BusAttachment mBus = new BusAttachment(APPNAME, RemoteMessage.Receive);
 		HelloInterface hello = new HelloImpl();
 		Status status = null;
-		
+
 		status = mBus.registerBusObject(hello, "/servicepath");
 		if (Status.OK != status) {
 			System.out.println("BusAttachment.registerBusObject() failed: "
@@ -26,7 +29,7 @@ public class Start {
 			return;
 		}
 
-		status= mBus.connect();
+		status = mBus.connect();
 		if (Status.OK != status) {
 			System.out.println("BusAttachment.connect() failed: " + status);
 
@@ -36,7 +39,28 @@ public class Start {
 			System.out.println("connect OK");
 		}
 		System.out.println(status);
-		
+
+		final short CONTACT_PORT = 42;
+		Mutable.ShortValue contactPort = new Mutable.ShortValue(CONTACT_PORT);
+		SessionOpts sessionOpts = new SessionOpts();
+		sessionOpts.traffic = SessionOpts.TRAFFIC_MESSAGES;
+		sessionOpts.isMultipoint = false;
+		sessionOpts.proximity = SessionOpts.PROXIMITY_ANY;
+		sessionOpts.transports = SessionOpts.TRANSPORT_ANY;
+
+		status = mBus.bindSessionPort(contactPort, sessionOpts,
+				new SessionPortListener() {
+					@Override
+					public boolean acceptSessionJoiner(short sessionPort,
+							String joiner, SessionOpts sessionOpts) {
+						if (sessionPort == CONTACT_PORT) {
+							return true;
+						} else {
+							return false;
+						}
+					}
+				});
+
 		status = mBus.requestName(SERVICE_NAME, 0);
 		if (Status.OK != status) {
 			System.out.println("BusAttachment.requestName() failed: " + status);
@@ -47,37 +71,52 @@ public class Start {
 			System.out.println("requestName OK");
 		}
 		System.out.println("we have a bus... ?");
-		
+
+		status = mBus.advertiseName(SERVICE_NAME, SessionOpts.TRANSPORT_ANY);
+		if (status != Status.OK) {
+			/*
+			 * If we are unable to advertise the name, release the well-known
+			 * name from the local bus.
+			 */
+			mBus.releaseName(SERVICE_NAME);
+			System.exit(0);
+			return;
+		}
+		if (status != Status.OK) {
+			System.exit(0);
+			return;
+		}
+
 		Client client = new Client();
 		client.fireClient();
 	}
-	
+
 	static class Client {
-		public void fireClient(){
+		public void fireClient() {
 			BusAttachment mBus = new BusAttachment(APPNAME);
 
 			Status status = mBus.connect();
-			if (Status.OK != status) { 
-			   System.out.println("BusAttachment.connect() failed:" + status); 
-			   System.exit(0);
-			   return;
+			if (Status.OK != status) {
+				System.out.println("BusAttachment.connect() failed:" + status);
+				System.exit(0);
+				return;
 			}
 
-			ProxyBusObject mProxyObj = mBus.getProxyBusObject(SERVICE_NAME, 
-			   "/servicepath",
-			   BusAttachment.SESSION_ID_ANY,
-			   new Class[] { HelloInterface.class });
+			ProxyBusObject mProxyObj = mBus.getProxyBusObject(SERVICE_NAME,
+					"/servicepath", BusAttachment.SESSION_ID_ANY,
+					new Class[] { HelloInterface.class });
 
 			HelloInterface iface = mProxyObj.getInterface(HelloInterface.class);
 
 			status = mBus.registerSignalHandlers(this);
 			if (status != Status.OK) {
-			   System.out.println("BusAttachment.registerSignalHandlers() failed:" +
-			status); 
-			   System.exit(0);
-			   return;
+				System.out
+						.println("BusAttachment.registerSignalHandlers() failed:"
+								+ status);
+				System.exit(0);
+				return;
 			}
-			
+
 			try {
 				System.out.println(iface.GetMyProperty());
 			} catch (BusException e) {
